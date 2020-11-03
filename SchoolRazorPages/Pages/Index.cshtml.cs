39dbf3907b59f38aa80367a06f8ddd7b91f9a783
Zuhair Haroon Khan <zk222ac@gmail.com>
2020-11-03 14:39:01 +0100

    Add project files.

---
+++ SchoolRazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using SchoolRazorPages.Model;
+
+namespace SchoolRazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public Student Student { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            Student = new Student();
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Message"] = "here is a new Page";
+            //ViewData["student"] = new Student() {Id = 1,Name="alex",ImagePath="lion.jpg"};
+            Student = new Student() {Id = 1,Name="alex",ImagePath="/Images/lion.jpg"};
+
+        }
+    }
+}
